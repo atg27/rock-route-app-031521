9a030e552c2398f27c758038e3a25fcf06998599
Andrew <atg27@uw.edu>
2021-05-08 11:16:38 -0700

    update climbing route fetch and render

--- src/containers/ClimbingRoute.js
+++ src/containers/ClimbingRoute.js
@@ -3,29 +3,32 @@ import React, { Component } from 'react'
 export default class ClimbingRoute extends Component {
 
     state={
-        routes: []
+        route:{
+            difficulty:'',
+            name:'',
+            state:''
+        }
     }
 
     componentDidMount() {
-        console.log("fetching")
-        fetch('http://localhost:3001/routes/{params match}')
+        console.log("fetching specific route")
+        fetch(`http://localhost:3001/routes/${this.props.match.params.id}`)
         .then(res => res.json())
         .then(data => {
             console.log(data)
             this.setState({
-                routes: data 
+                route: data 
             })
         })
     } 
 
     render() {
 
-        const routeList = this.state.routes.map((route) => <li key={route.id}>{route.name} {route.difficulty}{/* will be own component  */}</li>)
-
         return (
             <div>
-                <h3>route info here</h3>
-                {routeList}
+                <h3>Route Info:</h3>
+            
+                {this.state.route.name}
             </div>
         )
     }
