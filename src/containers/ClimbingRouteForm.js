74b884a75321b3b079d2e8f4b38431c8a9bb4a03
Andrew <atg27@uw.edu>
2021-05-08 22:55:31 -0700

    finished route form

--- src/containers/ClimbingRouteForm.js
+++ src/containers/ClimbingRouteForm.js
@@ -23,6 +23,7 @@ export default class ClimbingRouteForm extends Component {
             },
             body: JSON.stringify(this.state)
         })
+        .then(res => res.json())
         .then(data => {
             console.log("New Route", data)
         })
@@ -36,7 +37,7 @@ export default class ClimbingRouteForm extends Component {
             <div>
                 <h3>Submit New Route Here:</h3>
                 <hr/>
-                <form onSubmit={this.onSubmit}>
+                <form onSubmit={this.handleSubmit}>
                     <label>Name:</label> 
                     <br/>
                     <input name="name" onChange={this.handleChange} value={this.state.name} type="text"></input>
@@ -44,6 +45,17 @@ export default class ClimbingRouteForm extends Component {
                     <label>State:</label> 
                     <br/>
                     <input name="state" onChange={this.handleChange} value={this.state.state} type="text"></input>
+                    
+                    {/* <select name="state" onSelect={this.handleChange}>
+                        <option value={this.state.state}>Arizona</option>
+                        <option value={this.state.state}>California</option>
+                        <option value={this.state.state}>Idaho</option>
+                        <option value={this.state.state}>Nevada</option>
+                        <option value={this.state.state}>Oregon</option>
+                        <option value={this.state.state}>Utah</option>
+                        <option value={this.state.state}>Washington</option>
+                    </select> */}
+                    
                     <br/>
                     <label>Difficulty:</label> 
                     <br/>
