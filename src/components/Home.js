1156cf3a6b47708c819c4d47022b351076e40c0e
Andrew <atg27@uw.edu>
2021-05-09 21:43:57 -0700

    added clear form, passign proj reqs

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
 const Home = () => {
+
     return (
         <div>
             <h1 style={{textAlign: 'center'}}>Rock Routes App!</h1>
             <hr/>
             <p style={{textAlign: 'center'}}>
-                This app is for browsing and adding rock climbing routes by state! 
-                <br/>Additionally, it allows users to keep track of climbed routes.
-                <br/>Please select one of the navigation links at the top to use this app.
+                This app is for browsing and adding rock climbing routes 
+                <br/>
+                <br/>Select the Routes Link at the top of the Nav Bar to see Routes
             </p>
         </div>
     )
--- src/containers/ClimbingRouteForm.js
+++ src/containers/ClimbingRouteForm.js
@@ -26,7 +26,10 @@ export default class ClimbingRouteForm extends Component {
         .then(res => res.json())
         .then(data => {
             console.log("New Route", data)
+            this.props.history.push('/climbingroutes')
         })
+        alert("Route Posted!")
+
     }
 
 
@@ -45,17 +48,6 @@ export default class ClimbingRouteForm extends Component {
                     <label>State:</label> 
                     <br/>
                     <input name="state" onChange={this.handleChange} value={this.state.state} type="text"></input>
-                    
-                    {/* <select name="state" onSelect={this.handleChange}>
-                        <option value={this.state.state}>Arizona</option>
-                        <option value={this.state.state}>California</option>
-                        <option value={this.state.state}>Idaho</option>
-                        <option value={this.state.state}>Nevada</option>
-                        <option value={this.state.state}>Oregon</option>
-                        <option value={this.state.state}>Utah</option>
-                        <option value={this.state.state}>Washington</option>
-                    </select> */}
-                    
                     <br/>
                     <label>Difficulty:</label> 
                     <br/>
