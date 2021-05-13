a7aeb41a19c0d04d02c0d5eb6f8bcf832c706a71
Andrew <atg27@uw.edu>
2021-05-13 08:24:41 -0700

    made style dry

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,15 @@ import React from 'react'
 
 const Home = () => {
 
+    const divStyle = {
+        textAlign: 'center'
+    }
+
     return (
-        <div>
-            <h1 style={{textAlign: 'center'}}>Rock Routes App!</h1>
+        <div style={divStyle}>
+            <h1>Rock Routes App!</h1>
             <hr/>
-            <p style={{textAlign: 'center'}}>
+            <p >
                 This app is for browsing and adding rock climbing routes 
                 <br/>
                 <br/>Select the Routes Link at the top of the Nav Bar to see Routes
